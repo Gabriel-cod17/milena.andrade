8ee80137b101fabbac9feca1545ef6a85ff3b9b7
Gabriel Felipe <gabrielssantiago1007@gmail.com>
2025-04-01 16:42:09 -0300

    Atualização

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,43 @@
+const liMobileSobre = document.querySelector("#liMobileSobre");
+const liMobileRedes = document.querySelector("#liMobileRedes");
+const liMobileServicos = document.querySelector("#liMobileServicos");
+let menuMobile = document.querySelector('.mobile-menu');
+
+liMobileSobre.addEventListener("click", () => {
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector(".icon").src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add("open");
+        document.querySelector(".icon").src = "assets/img/close_white_36dp.svg";
+    }
+})
+
+liMobileRedes.addEventListener("click", () => {
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector(".icon").src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add("open");
+        document.querySelector(".icon").src = "assets/img/close_white_36dp.svg";
+    }
+})
+
+liMobileServicos.addEventListener("click", () => {
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector(".icon").src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add("open");
+        document.querySelector(".icon").src = "assets/img/close_white_36dp.svg";
+    }
+})
+
+
+
+
 function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')){
+    if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
         document.querySelector(".icon").src = "assets/img/menu_white_36dp.svg";
     } else {
